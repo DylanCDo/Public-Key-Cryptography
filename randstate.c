d7c936b4bdcd936aa95d54ebd6d0817773f6603b
Dylan <82780555+DylanCDo@users.noreply.github.com>
2023-01-06 23:26:13 -0800

    Create randstate.c

---
+++ randstate.c
@@ -0,0 +1,15 @@
+#include "randstate.h"
+
+#include <stdint.h>
+#include <gmp.h>
+
+gmp_randstate_t state;
+
+void randstate_init(uint64_t seed) {
+    gmp_randinit_mt(state);
+    gmp_randseed_ui(state, seed);
+}
+
+void randstate_clear(void) {
+    gmp_randclear(state);
+}
